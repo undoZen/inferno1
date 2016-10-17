723c1d4226bc4e9c42c89f4e8fe909e532cfeb9e
Anson Yu Wang <opensource@undozen.com>
2016-10-17 21:52:30 +0800

    use official inferno-hyperscript

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ exports.Inferno = require('inferno')
 exports.InfernoDOM = require('inferno-dom')
 exports.createElement = require('inferno-create-element')
 exports.InfernoCompat = require('inferno-compat')
+exports.h = require('inferno-hyperscript')
 

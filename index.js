8d25e30e365eca6fc978852303c080565c67e2d5
Anson Yu Wang <opensource@undozen.com>
2016-10-17 21:36:14 +0800

    init @beta4

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.Inferno = require('inferno')
+exports.InfernoDOM = require('inferno-dom')
+exports.createElement = require('inferno-create-element')
+exports.InfernoCompat = require('inferno-compat')
+
